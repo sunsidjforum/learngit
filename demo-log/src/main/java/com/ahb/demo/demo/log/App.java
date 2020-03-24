ff61963fa535e31bc1789cad499a9a2ea7898b47
sunsi <499907956@qq.com>
2020-03-24 10:38:48 +0800

    first commit for jenkins test

---
+++ demo-log/src/main/java/com/ahb/demo/demo/log/App.java
@@ -0,0 +1,13 @@
+package com.ahb.demo.demo.log;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
